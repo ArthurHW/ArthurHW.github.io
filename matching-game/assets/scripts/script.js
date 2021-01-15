64d618cf1ebf289e67ad96bdcb267b78475b3f6d
Arthur Henrique Wiebusch <arturh07@gmail.com>
2021-01-15 19:47:45 -0300

    Aprimoring Matching Game Theme Switcher

--- matching-game/assets/scripts/script.js
+++ matching-game/assets/scripts/script.js
@@ -2,31 +2,67 @@ const FRONT = "card_front"
 const BACK = "card_back"
 const CARD = "card"
 const ICON = "icon"
+let theme
 
 function switchTheme(){
+
+    if (theme == 'white'){
+        theme = 'black'
+    } else{
+        theme = 'white'
+    }
+    localStorage.setItem('theme', theme)
+
+    updateTheme()
+}
+
+function updateTheme(){
+
     let body = document.getElementsByTagName('body')[0]
     let title = document.getElementById("title")
-    let cardBack = document.getElementsByClassName("card_back")
+    let cards = document.getElementsByClassName("card")
     let header = document.getElementById("header")
     let themeSwitcher = document.getElementById("theme-switcher")
 
-    themeSwitcher.classList.toggle("dark")
-    header.classList.toggle("dark")
-    for(card of cardBack){
-        card.classList.toggle("dark")
-    }
-    title.classList.toggle("dark")
-    body.classList.toggle("dark")
-    console.log(body)
+    if (theme == 'white'){
 
+        themeSwitcher.classList.remove("dark")
+        header.classList.remove("dark")
+        for(card of cards){
+            card.classList.remove("dark")
+        }
+        title.classList.remove("dark")
+        body.classList.remove("dark")
+        console.log('a')
+
+    } else{
+        themeSwitcher.classList.add("dark")
+        header.classList.add("dark")
+        for(card of cards){
+            card.classList.add("dark")
+        }
+        title.classList.add("dark")
+        body.classList.add("dark")
+    }
 }
 
 
 startGame()
 
 function startGame(){
+
+    theme = localStorage.getItem('theme')
     
     initializeCards(game.createCardsFromTechs())
+    console.log(theme)
+    if (theme == null){
+        theme = 'white'
+        localStorage.setItem('theme', theme)
+    }
+
+    updateTheme()
+
+
 }
 
 function initializeCards(){
@@ -39,6 +75,9 @@ function initializeCards(){
         let cardElement = document.createElement('div')
         cardElement.id = card.id
         cardElement.classList.add(CARD)
+        if (theme == 'black'){
+            cardElement.classList.add('dark')
+        }
         cardElement.dataset.icon = card.icon
 
         createCardContent(card, cardElement)
