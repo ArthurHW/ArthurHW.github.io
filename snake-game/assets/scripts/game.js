f3149f2e7774d461b80f85e4ff6be8bb4cbdf047
Arthur Henrique Wiebusch <arturh07@gmail.com>
2021-05-31 15:59:08 -0300

    Revert "transferindo snake-game para outro repositorio"
    This reverts commit 5367b6a024f401fe71bb24e157bdfc0b8ba7b12f.

---
+++ snake-game/assets/scripts/game.js
@@ -0,0 +1,87 @@
+import { update as updateSnake, draw as drawSnake, SNAKE_SPEED, getSnakeHead, snakeIntersection} from "./snake.js"
+import { update as updateFood, draw as drawFood} from "./food.js"
+import { outsideGrid } from "./grid.js"
+
+let gameOverLayer = document.getElementById('gameOverScreen')
+let restartButton = document.getElementById('restartButton')
+let score = 0
+let topScore = localStorage.getItem('topScoreSnakeGame')
+let scoreDisplay = document.getElementById('scoreDisplay')
+let topScoreDisplay = document.getElementById('topScoreDisplay')
+
+let lastRenderTime = 0
+let gameOver = false
+const gameBoard = document.getElementById("game-board")
+
+if (topScore == null){
+    topScoreDisplay.innerHTML = 'HIGHEST SCORE: 0'
+} else{
+    topScoreDisplay.innerHTML = 'HIGHEST SCORE: ' + topScore
+}
+
+
+function main(currentTime){
+
+    if (gameOver){
+        gameOverLayer.style.display = 'flex'
+        return
+    }
+
+    window.requestAnimationFrame(main)
+    const secondSinceLastRender = (currentTime - lastRenderTime) / 1000
+    if (secondSinceLastRender < 1 / SNAKE_SPEED) return
+
+    lastRenderTime = currentTime
+    
+    update()
+    draw()
+}
+
+
+window.requestAnimationFrame(main)
+
+function update(){
+    updateSnake()
+    if (updateFood()) {
+        updateScore()
+    }
+    checkDeath()
+}
+
+function draw(){
+    gameBoard.innerHTML = ''
+    drawSnake(gameBoard)
+    drawFood(gameBoard)
+}
+
+function checkDeath(){
+    gameOver = outsideGrid(getSnakeHead()) || snakeIntersection()
+}
+
+restartButton.addEventListener('click', () => {
+    window.location = './'
+})
+
+function updateTopScore(){
+    topScore = score
+    localStorage.setItem('topScoreSnakeGame', topScore)
+    topScoreDisplay.innerHTML = 'HIGHEST SCORE: ' + topScore
+}
+
+function updateScore(){
+    score++
+    if (score > topScore){
+        updateTopScore()
+    }
+    scoreDisplay.innerHTML = 'SCORE: ' + score.toString()
+}
+
+
+
+
+
+
+
+
+
+
